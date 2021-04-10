cdab7838ad7f4c5f9a4070f2ceed2a767a73ab20
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-04-10 15:09:52 +0700

    Исправил функционал изменения размеров toolbar на списке изображений

--- dholo/dholo/dhimgexpr.cpp
+++ dholo/dholo/dhimgexpr.cpp
@@ -21,47 +21,50 @@ BEGIN_MESSAGE_MAP(DHImgExpr, CDockablePane)
 	ON_WM_CREATE()
 	ON_WM_SIZE()
 	ON_WM_CONTEXTMENU()
+	ON_COMMAND(ID_EDIT_CUT, OnEditCut)
+	ON_COMMAND(ID_EDIT_COPY, OnEditCopy)
+	ON_COMMAND(ID_EDIT_CLEAR, OnEditClear)
 	ON_WM_PAINT()
 	ON_WM_SETFOCUS()
-	//ON_COMMAND(ID_ADD_IMAGE, OnAddImage)
 END_MESSAGE_MAP()
 
-void DHImgExpr::AddJustLayout()
-{
-	if (GetSafeHwnd() == nullptr)
-	{
-		return;
-	}
-
-	CRect rectClient;
-	GetClientRect(rectClient);
-
-	int cyTlb = m_toolBar.CalcFixedLayout(FALSE, TRUE).cy;
-
-	m_toolBar.SetWindowPos(nullptr, rectClient.left, rectClient.top, rectClient.Width(), cyTlb, SWP_NOACTIVATE | SWP_NOZORDER);
-}
-
-void DHImgExpr::OnChangeVisualStyle()
-{
-}
+/////////////////////////////////////////////////////////////////////////////
+// обработчики сообщений CWorkspaceBar
 
 int DHImgExpr::OnCreate(LPCREATESTRUCT lpCreateStruct)
 {
 	if (CDockablePane::OnCreate(lpCreateStruct) == -1)
 		return -1;
 
-	m_toolBar.Create(this, AFX_DEFAULT_TOOLBAR_STYLE, IDR_EXPLORER);
-	m_toolBar.LoadToolBar(IDR_EXPLORER);
+	CRect rectDummy;
+	rectDummy.SetRectEmpty();
 
-	//m_toolBar.SetPaneStyle(m_toolBar.GetPaneStyle() | CBRS_TOOLTIPS | CBRS_FLYBY);
-	
-	//m_toolBar.SetPaneStyle(m_toolBar.GetPaneStyle() & ~(CBRS_GRIPPER | CBRS_SIZE_DYNAMIC | CBRS_BORDER_TOP | CBRS_BORDER_BOTTOM | CBRS_BORDER_LEFT | CBRS_BORDER_RIGHT));
+	// Создание представления:
+	const DWORD dwViewStyle = WS_CHILD | WS_VISIBLE | LVS_ICON | LVS_REPORT;
 
-	m_toolBar.SetOwner(this);
+	//if (!m_List.Create(dwViewStyle, rectDummy, this, 4))
+	//{
+	//	TRACE0("Не удалось создать представление файлов\n");
+	//	return -1;      // не удалось создать
+	//}
+	//
+	//m_ImageList.Create(64, 64, ILC_COLOR24 | ILC_MASK, 8, 4);
 
-//	m_toolBar.SetRouteCommandsViaFrame(FALSE);
+	m_wndToolBar.Create(this, AFX_DEFAULT_TOOLBAR_STYLE, IDR_EXPLORER);
+	m_wndToolBar.LoadToolBar(IDR_EXPLORER, 0, 0, TRUE /* Заблокирован */);
 
-	AddJustLayout();
+	m_wndToolBar.SetPaneStyle(m_wndToolBar.GetPaneStyle() | CBRS_TOOLTIPS | CBRS_FLYBY);
+
+	m_wndToolBar.SetPaneStyle(m_wndToolBar.GetPaneStyle() & ~(CBRS_GRIPPER | CBRS_SIZE_DYNAMIC | CBRS_BORDER_TOP | CBRS_BORDER_BOTTOM | CBRS_BORDER_LEFT | CBRS_BORDER_RIGHT));
+
+	m_wndToolBar.SetOwner(this);
+
+	// Все команды будут перенаправлены через этот элемент управления, а не через родительскую рамку:
+	m_wndToolBar.SetRouteCommandsViaFrame(FALSE);
+
+	// Введите некоторые данные статического представления в виде дерева (пустой код, ничего более)
+	FillFileView();
+	AdjustLayout();
 
 	return 0;
 }
@@ -72,19 +75,176 @@ void DHImgExpr::OnSize(UINT nType, int cx, int cy)
 	AdjustLayout();
 }
 
-void DHImgExpr::OnContextMenu(CWnd * pWnd, CPoint point)
+void DHImgExpr::FillFileView()
 {
+	//int Index;
+	//BOOL validString;
+	//
+	//CString columnImage;
+	//validString = columnImage.LoadStringW(IDS_IMAGE_LIST_COLUMN_IMAGE);
+	//if (!validString)
+	//	return;
+	//
+	//m_List.InsertColumn(0, columnImage, LVCFMT_LEFT);
+	//
+	//CString columnPath;
+	//validString = columnPath.LoadStringW(IDS_IMAGE_LIST_COLUMN_PATH);
+	//if (!validString)
+	//	return;
+	//
+	//m_List.InsertColumn(1, columnPath, LVCFMT_LEFT);
+	//
+	//m_List.SetColumnWidth(0, COLUMN_IMAGE_WIDTH);
+	//m_List.SetColumnWidth(1, COLUMN_PATH_WIDTH);
 
+}
+
+void DHImgExpr::AddImage(CStringW pathImage, CStringW imageName, CStringW imageExt)
+{
+	//if (!pathImage || !imageName || !imageExt)
+	//	return;
+	//HRESULT res;
+	//CImage m_LoaderImageObject;
+	//CBitmap m_BitmapImageObject;
+	//res = m_LoaderImageObject.Load(pathImage);
+	//
+	//m_BitmapImageObject.Attach((HBITMAP)m_LoaderImageObject);
+	//m_ImageList.Add(&m_BitmapImageObject, RGB(0x00, 0x00, 0x00));
+	//
+	//m_List.SetImageList(&m_ImageList, LVSIL_NORMAL);
+	//int imageIndex = m_ImageList.Add(&m_BitmapImageObject, RGB(0xff, 0xff, 0xff));
+	//
+	//m_List.SetImageList(&m_ImageList, LVSIL_SMALL);
+	//
+	//int listIndex;
+	//LV_ITEM lvItem;
+	//listIndex = m_List.GetItemCount() + 1;
+	//lvItem.mask = LVIF_IMAGE | LVIF_TEXT;
+	//lvItem.iItem = listIndex;
+	//lvItem.iSubItem = 0;
+	//lvItem.iImage = 0;
+	//lvItem.iImage = imageIndex;
+	//lvItem.pszText = const_cast<LPWSTR>(imageName.GetString());
+	//
+	//listIndex = m_List.InsertItem(&lvItem);
+	//lvItem.mask = LVIF_TEXT;
+	//lvItem.iItem = listIndex;
+	//lvItem.iSubItem = 1;
+	//lvItem.iImage = 0;
+	//lvItem.pszText = const_cast<LPWSTR>(pathImage.GetString());
+	//m_List.SetItem(&lvItem);
+}
+
+void DHImgExpr::OnContextMenu(CWnd* pWnd, CPoint point)
+{
+	//CTreeCtrl* pWndTree = (CTreeCtrl*)&m_List;
+	//ASSERT_VALID(pWndTree);
+	//
+	//if (pWnd != pWndTree)
+	//{
+	//	CDockablePane::OnContextMenu(pWnd, point);
+	//	return;
+	//}
+	//
+	//if (point != CPoint(-1, -1))
+	//{
+	//	// Выбрать нажатый элемент:
+	//	CPoint ptTree = point;
+	//	pWndTree->ScreenToClient(&ptTree);
+	//
+	//	UINT flags = 0;
+	//	HTREEITEM hTreeItem = pWndTree->HitTest(ptTree, &flags);
+	//	if (hTreeItem != nullptr)
+	//	{
+	//		pWndTree->SelectItem(hTreeItem);
+	//	}
+	//}
+	//
+	//pWndTree->SetFocus();
+	//theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EXPLORER, point.x, point.y, this, TRUE);
+}
+
+void DHImgExpr::AdjustLayout()
+{
+	if (GetSafeHwnd() == nullptr)
+	{
+		return;
+	}
+
+	CRect rectClient;
+	GetClientRect(rectClient);
+
+	int cyTlb = m_wndToolBar.CalcFixedLayout(FALSE, TRUE).cy;
+
+	m_wndToolBar.SetWindowPos(nullptr, rectClient.left, rectClient.top, rectClient.Width(), cyTlb, SWP_NOACTIVATE | SWP_NOZORDER);
+	//m_List.SetWindowPos(nullptr, rectClient.left + 1, rectClient.top + cyTlb + 1, rectClient.Width() - 2, rectClient.Height() - cyTlb - 2, SWP_NOACTIVATE | SWP_NOZORDER);
 }
 
 void DHImgExpr::OnProperties()
 {
+	OnLoadImage();
+}
+
+void DHImgExpr::OnFileOpen()
+{
+	OnLoadImage();
+}
+
+void DHImgExpr::OnFileOpenWith()
+{
 
 }
 
-void DHImgExpr::OnAddImage()
+void DHImgExpr::OnCreateNew()
 {
+	//if (!m_List.GetItemCount())
+	//	return;
+	//
+	//POSITION pos = m_List.GetFirstSelectedItemPosition();
+	//
+	//if (pos == 0)
+	//	return;
+	//
+	//UINT SelectedItem = m_List.GetNextSelectedItem(pos);
+	//CString ImagePath = m_List.GetItemText(SelectedItem, 1);
+	//
+	//theApp.OnFileNew();
+	//
+	//DigitalHologramFrame* pMdiActiveFrame =
+	//	static_cast<DigitalHologramFrame*>(
+	//		static_cast<CMainFrame*>(theApp.m_pMainWnd)->MDIGetActive());
+	//
+	//if (!pMdiActiveFrame)
+	//	return;
+	//
+	//if (!pMdiActiveFrame->IsKindOf(RUNTIME_CLASS(DigitalHologramFrame)))
+	//	return;
+	//
+	//CDigitalHologramView* mHoloView =
+	//	static_cast<CDigitalHologramView*>(pMdiActiveFrame->GetActiveView());
+	//
+	//if (!mHoloView)
+	//	return;
+	//
+	//if (!mHoloView->IsKindOf(RUNTIME_CLASS(CDigitalHologramView)))
+	//	return;
+	//
+	//mHoloView->SetImagePath(ImagePath);
+}
 
+void DHImgExpr::OnEditCut()
+{
+	// TODO: добавьте сюда код обработчика команд
+}
+
+void DHImgExpr::OnEditCopy()
+{
+	// TODO: добавьте сюда код обработчика команд
+}
+
+void DHImgExpr::OnEditClear()
+{
+	// TODO: добавьте сюда код обработчика команд
 }
 
 void DHImgExpr::OnPaint()
@@ -99,7 +259,59 @@ void DHImgExpr::OnPaint()
 	dc.Draw3dRect(rectTree, ::GetSysColor(COLOR_3DSHADOW), ::GetSysColor(COLOR_3DSHADOW));
 }
 
-void DHImgExpr::OnSetFocus(CWnd * pOldWnd)
+void DHImgExpr::OnSetFocus(CWnd* pOldWnd)
 {
 	CDockablePane::OnSetFocus(pOldWnd);
+
+	//m_List.SetFocus();
 }
+
+void DHImgExpr::OnChangeVisualStyle()
+{
+	//m_wndToolBar.CleanUpLockedImages();
+	//m_wndToolBar.LoadBitmap(theApp.m_bHiColorIcons ? IDB_EXPLORER_24 : IDR_EXPLORER, 0, 0, TRUE /* Заблокирован */);
+	//
+	//m_ImageList.DeleteImageList();
+	//
+	//UINT uiBmpId = theApp.m_bHiColorIcons ? IDB_FILE_VIEW_24 : IDB_FILE_VIEW;
+	//
+	//CBitmap bmp;
+	//if (!bmp.LoadBitmap(uiBmpId))
+	//{
+	//	TRACE(_T("Не удается загрузить точечный рисунок: %x\n"), uiBmpId);
+	//	ASSERT(FALSE);
+	//	return;
+	//}
+	//
+	//BITMAP bmpObj;
+	//bmp.GetBitmap(&bmpObj);
+
+	//UINT nFlags = ILC_MASK;
+	//
+	//nFlags |= (theApp.m_bHiColorIcons) ? ILC_COLOR24 : ILC_COLOR4;
+}
+
+void DHImgExpr::OnLoadImage()
+{
+	CFileDialog dlg(true);
+
+	if (dlg.DoModal() == IDOK)
+		AddImage(dlg.GetPathName(), dlg.GetFileName(), dlg.GetFileExt());
+}
+
+void DHImgExpr::OnUpdateDummyCompile(CCmdUI* pCmdUI)
+{
+	//if (m_List.GetItemCount() == 0)
+	//{
+	//	pCmdUI->Enable(false);
+	//	return;
+	//}
+
+	//if (!m_List.GetSelectedCount())
+	//{
+	//	pCmdUI->Enable(false);
+	//	return;
+	//}
+	//
+	//pCmdUI->Enable(true);
+}	//
--- dholo/dholo/dhimgexpr.h
+++ dholo/dholo/dhimgexpr.h
@@ -1,10 +1,10 @@
 #pragma once
 
-class DHImgExprToolBar : public CToolBar
+class DHToolBar : public CMFCToolBar
 {
-	virtual void OnUpdateCmdUI(CFrameWnd* pTarget, BOOL bDisableIfNoHndler)
+	virtual void OnUpdateCmdUI(CFrameWnd* /*pTarget*/, BOOL bDisableIfNoHndler)
 	{
-		CToolBar::OnUpdateCmdUI((CFrameWnd*)GetOwner(), bDisableIfNoHndler);
+		CMFCToolBar::OnUpdateCmdUI((CFrameWnd*)GetOwner(), bDisableIfNoHndler);
 	}
 
 	virtual BOOL AllowShowOnList() const { return FALSE; }
@@ -12,32 +12,42 @@ class DHImgExprToolBar : public CToolBar
 
 class DHImgExpr : public CDockablePane
 {
-// Создание
+	// Создание
 public:
 	DHImgExpr() noexcept;
-	~DHImgExpr();
+	virtual ~DHImgExpr();
 
-	void AddJustLayout();
+	void AdjustLayout();
 	void OnChangeVisualStyle();
-	
-// Переопределение функций базовго класса
+	void AddImage(CStringW pathImage, CStringW imageName, CStringW imageExt);
+
+private:
+	void OnLoadImage();
+
+protected:
+	void FillFileView();
+
 protected:
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
 	afx_msg void OnProperties();
+	afx_msg void OnFileOpen();
+	afx_msg void OnFileOpenWith();
+	afx_msg void OnCreateNew();
+	afx_msg void OnEditCut();
+	afx_msg void OnEditCopy();
+	afx_msg void OnEditClear();
 	afx_msg void OnPaint();
 	afx_msg void OnSetFocus(CWnd* pOldWnd);
 
-	afx_msg void OnAddImage();
-
 	DECLARE_MESSAGE_MAP()
 
-// Приватные члены для создания 
 private:
-	DHImgExprToolBar m_toolBar;
+	CImageList m_ImageList;
+	DHToolBar m_wndToolBar;
 
-// Приватные члены для работы с изображениями
-private:
+public:
+	afx_msg void OnUpdateDummyCompile(CCmdUI* pCmdUI);
 };
 
