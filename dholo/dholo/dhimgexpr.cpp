e1e40f2541f5a112433c689bf41a4eddbce090ac
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-04-10 13:27:14 +0700

    Добавил менеджер изображения и toolbar для него

---
+++ dholo/dholo/dhimgexpr.cpp
@@ -0,0 +1,92 @@
+#include "stdafx.h"
+#include "dhimgexpr.h"
+#include "resource.h"
+#include "dhmainfrm.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#define new DEBUG_NEW
+#endif
+
+DHImgExpr::DHImgExpr() noexcept
+{
+}
+
+
+DHImgExpr::~DHImgExpr()
+{
+}
+
+BEGIN_MESSAGE_MAP(DHImgExpr, CDockablePane)
+	ON_WM_CREATE()
+	ON_WM_SIZE()
+	ON_WM_CONTEXTMENU()
+	ON_WM_PAINT()
+	ON_WM_SETFOCUS()
+END_MESSAGE_MAP()
+
+void DHImgExpr::AddJustLayout()
+{
+	if (GetSafeHwnd() == nullptr)
+	{
+		return;
+	}
+
+	CRect rectClient;
+	GetClientRect(rectClient);
+
+	int cyTlb = m_toolBar.CalcFixedLayout(FALSE, TRUE).cy;
+
+	m_toolBar.SetWindowPos(nullptr, rectClient.left, rectClient.top, rectClient.Width(), cyTlb, SWP_NOACTIVATE | SWP_NOZORDER);
+}
+
+void DHImgExpr::OnChangeVisualStyle()
+{
+}
+
+int DHImgExpr::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CDockablePane::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	m_toolBar.Create(this, AFX_DEFAULT_TOOLBAR_STYLE, IDR_EXPLORER);
+	m_toolBar.LoadToolBar(IDR_EXPLORER);
+
+	m_toolBar.SetOwner(this);
+
+//	m_toolBar.SetRouteCommandsViaFrame(FALSE);
+
+	return 0;
+}
+
+void DHImgExpr::OnSize(UINT nType, int cx, int cy)
+{
+	CDockablePane::OnSize(nType, cx, cy);
+	AdjustLayout();
+}
+
+void DHImgExpr::OnContextMenu(CWnd * pWnd, CPoint point)
+{
+}
+
+void DHImgExpr::OnProperties()
+{
+}
+
+void DHImgExpr::OnPaint()
+{
+	CPaintDC dc(this); // контекст устройства для рисования
+
+	CRect rectTree;
+	//m_List.GetWindowRect(rectTree);
+	ScreenToClient(rectTree);
+
+	rectTree.InflateRect(1, 1);
+	dc.Draw3dRect(rectTree, ::GetSysColor(COLOR_3DSHADOW), ::GetSysColor(COLOR_3DSHADOW));
+}
+
+void DHImgExpr::OnSetFocus(CWnd * pOldWnd)
+{
+	CDockablePane::OnSetFocus(pOldWnd);
+}
---
+++ dholo/dholo/dhimgexpr.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class DHImgExprToolBar : public CToolBar
+{
+	virtual void OnUpdateCmdUI(CFrameWnd* pTarget, BOOL bDisableIfNoHndler)
+	{
+		CToolBar::OnUpdateCmdUI((CFrameWnd*)GetOwner(), bDisableIfNoHndler);
+	}
+
+	virtual BOOL AllowShowOnList() const { return FALSE; }
+};
+
+class DHImgExpr : public CDockablePane
+{
+// Создание
+public:
+	DHImgExpr() noexcept;
+	~DHImgExpr();
+
+	void AddJustLayout();
+	void OnChangeVisualStyle();
+	
+// Переопределение функций базовго класса
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	afx_msg void OnProperties();
+	afx_msg void OnPaint();
+	afx_msg void OnSetFocus(CWnd* pOldWnd);
+
+	DECLARE_MESSAGE_MAP()
+
+// Приватные члены для создания 
+private:
+	DHImgExprToolBar m_toolBar;
+
+// Приватные члены для работы с изображениями
+private:
+};
+
