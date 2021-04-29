11ee9b6623704eb0a550dc0b68a480c16d07495a
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-04-29 20:18:35 +0700

    Проблемы с файлом ресурсов, буду создавать новый проект и переносить исходник, блэт

---
+++ dholo/digital-hologram/ChildFrm.h
@@ -0,0 +1,43 @@
+﻿
+// ChildFrm.h: интерфейс класса CChildFrame
+//
+
+#pragma once
+#include "ChildView.h"
+
+class CChildFrame : public CMDIChildWndEx
+{
+	DECLARE_DYNCREATE(CChildFrame)
+public:
+	CChildFrame() noexcept;
+
+// Атрибуты
+protected:
+	CSplitterWnd m_wndSplitter;
+public:
+
+// Операции
+public:
+
+// Переопределение
+	public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	virtual BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo);
+
+// Реализация
+public:
+	// представление для клиентской области рамки.
+	CChildView m_wndView;
+	virtual ~CChildFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+// Созданные функции схемы сообщений
+protected:
+	afx_msg void OnFileClose();
+	afx_msg void OnSetFocus(CWnd* pOldWnd);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	DECLARE_MESSAGE_MAP()
+};
