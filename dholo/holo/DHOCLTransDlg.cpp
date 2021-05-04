a6224104e9ab5f06ba583261e553bb01b0df6e37
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-05-04 14:19:43 +0700

    Добавил новый класс для обработки диалога обработки

---
+++ dholo/holo/DHOCLTransDlg.cpp
@@ -0,0 +1,83 @@
+﻿#include "pch.h"
+#include "DHOCLTransDlg.h"
+#include "afxdialogex.h"
+#include "resource.h"
+
+namespace dholo
+{
+	IMPLEMENT_DYNAMIC(DHOCLTransDlg, CDialogEx)
+
+		DHOCLTransDlg::DHOCLTransDlg(CWnd* pParent /*=nullptr*/)
+		: CDialogEx(IDD_OCL_TRANSFORM, pParent)
+	{
+
+	}
+
+	DHOCLTransDlg::~DHOCLTransDlg()
+	{
+	}
+
+	void DHOCLTransDlg::DoDataExchange(CDataExchange* pDX)
+	{
+		CDialogEx::DoDataExchange(pDX);
+	}
+
+
+	BEGIN_MESSAGE_MAP(DHOCLTransDlg, CDialogEx)
+		ON_CBN_SELCHANGE(IDC_COMBO_PLATFORM_NAME,
+			&DHOCLTransDlg::OnCbnSelchangeComboPlatformName)
+
+		ON_CBN_SELCHANGE(IDC_COMBO_DEVICE_NAME,
+			&DHOCLTransDlg::OnCbnSelchangeComboDeviceName)
+	END_MESSAGE_MAP()
+
+
+	BOOL DHOCLTransDlg::OnInitDialog()
+	{
+		if (!CDialogEx::OnInitDialog())
+			return FALSE;
+
+		//if (!m_Init.Init())
+		//	return FALSE;
+
+		FillInComboPlatforms();
+		FillInPlatformPanel();
+
+		FillInComboDevice();
+		FillInDevicePanel();
+
+		return TRUE;
+	}
+
+	void DHOCLTransDlg::OnOK()
+	{
+		int nPlatform = \
+			static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_PLATFORM_NAME))->GetCurSel();
+		int nDevice = \
+			static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_DEVICE_NAME))->GetCurSel();
+
+		auto hard = m_Init.GetHardware(nPlatform);
+		m_Host.SetPlatform(hard.GetPlatform());
+		m_Host.SetDevice(hard.GetDevice(nDevice));
+
+		CDialogEx::OnOK();
+	}
+
+	void DHOCLTransDlg::OnCancel()
+	{
+		CDialogEx::OnCancel();
+	}
+
+
+	void DHOCLTransDlg::OnCbnSelchangeComboPlatformName()
+	{
+		// TODO: добавьте свой код обработчика уведомлений
+	}
+
+
+	void DHOCLTransDlg::OnCbnSelchangeComboDeviceName()
+	{
+		// TODO: добавьте свой код обработчика уведомлений
+	}
+
+}
---
+++ dholo/holo/DHOCLTransDlg.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "DHOCLInit.h"
+
+namespace dholo {
+	class DHOCLTransDlg : public CDialogEx
+	{
+		DECLARE_DYNAMIC(DHOCLTransDlg)
+
+	public:
+		DHOCLTransDlg(CWnd* pParent = nullptr);
+		virtual ~DHOCLTransDlg();
+
+		virtual void DoDataExchange(CDataExchange* pDX);
+
+		afx_msg BOOL OnInitDialog();
+
+		afx_msg void OnOK();
+		afx_msg void OnCancel();
+
+		afx_msg void OnCbnSelchangeComboPlatformName();
+		afx_msg void OnCbnSelchangeComboDeviceName();
+
+		void FillInComboPlatforms();
+		void FillInPlatformPanel();
+
+		void FillInComboDevice();
+		void FillInDevicePanel();
+
+		DECLARE_MESSAGE_MAP()
+
+	private:
+		ocl::DHOCLInit m_Init;
+		ocl::DHOCLHost m_Host;
+
+	};
+}
