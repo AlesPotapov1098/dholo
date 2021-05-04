9f6d30000ebf23864e1907d52cf2518de901ad1e
AlesPotapov1098 <awesome.ales1098@yandex.ru>
2021-05-04 14:31:24 +0700

    Добавил методы в класс обработки диалогового окна

--- dholo/holo/DHOCLTransDlg.cpp
+++ dholo/holo/DHOCLTransDlg.cpp
@@ -71,13 +71,101 @@ namespace dholo
 
 	void DHOCLTransDlg::OnCbnSelchangeComboPlatformName()
 	{
-		// TODO: добавьте свой код обработчика уведомлений
+		FillInPlatformPanel();
+		FillInDevicePanel();
 	}
 
 
 	void DHOCLTransDlg::OnCbnSelchangeComboDeviceName()
 	{
-		// TODO: добавьте свой код обработчика уведомлений
+		FillInDevicePanel();
+	}
+
+	void DHOCLTransDlg::FillInComboPlatforms()
+	{
+		auto comboBox = static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_PLATFORM_NAME));
+		ASSERT(comboBox != nullptr);
+
+		if (comboBox->GetCount() != 0)
+			comboBox->ResetContent();
+
+		int countHard = m_Init.GetSize();
+		ocl::DHOCLHard hard;
+		ocl::DHOCLInfo info;
+
+		for (int i = 0; i < countHard; i++)
+		{
+			hard = m_Init.GetHardware(i);
+			int countDevs = hard.GetCountDevices();
+
+			for (int j = 0; j < countDevs; j++)
+				comboBox->AddString(info.GetPlatformName(hard.GetPlatform()).c_str());
+		}
+
+		comboBox->SetCurSel(0);
+	}
+
+	void DHOCLTransDlg::FillInPlatformPanel()
+	{
+		auto comboBox = static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_PLATFORM_NAME));
+
+		int CurSell = comboBox->GetCurSel();
+		ocl::DHOCLHard hard = m_Init.GetHardware(CurSell);
+		ocl::DHOCLInfo info;
+
+		auto editPlatformVendor =
+			static_cast<CEdit*>(this->GetDlgItem(IDC_EDIT_PLATFORM_VENDOR));
+		editPlatformVendor->SetWindowTextW(info.GetPlatformVendor(hard.GetPlatform()).c_str());
+
+		auto editPlatformVersion =
+			static_cast<CEdit*>(this->GetDlgItem(IDC_EDIT_PLATFORM_VERSION));
+		editPlatformVersion->SetWindowTextW(info.GetPlatformVersion(hard.GetPlatform()).c_str());
+	}
+
+	void DHOCLTransDlg::FillInComboDevice()
+	{
+		auto comboBox =
+			static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_PLATFORM_NAME));
+
+		int curSell = comboBox->GetCurSel();
+		ocl::DHOCLHard hard = m_Init.GetHardware(curSell);
+		ocl::DHOCLInfo info;
+
+		auto comboDevices =
+			static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_DEVICE_NAME));
+		int countDevs = hard.GetCountDevices();
+
+		for (int i = 0; i < countDevs; i++)
+			comboDevices->AddString(info.GetDeviceName(hard[i]).c_str());
+
+		comboDevices->SetCurSel(0);
+	}
+
+	void DHOCLTransDlg::FillInDevicePanel()
+	{
+		auto comboBox =
+			static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_PLATFORM_NAME));
+
+		int curSell = comboBox->GetCurSel();
+		ocl::DHOCLHard hard = m_Init.GetHardware(curSell);
+		ocl::DHOCLInfo info;
+
+		auto comboDevice =
+			static_cast<CComboBox*>(this->GetDlgItem(IDC_COMBO_DEVICE_NAME));
+
+		int curSellComboDevices = comboDevice->GetCurSel();
+
+		auto editDeviceVendor =
+			static_cast<CEdit*>(this->GetDlgItem(IDC_EDIT_DEVICE_VENDOR));
+		editDeviceVendor->SetWindowTextW(info.GetDeviceVendor(hard[curSellComboDevices]).c_str());
+
+		auto editDeviceVersion =
+			static_cast<CEdit*>(this->GetDlgItem(IDC_EDIT_DEVICE_VERSION));
+		editDeviceVersion->SetWindowTextW(info.GetDeviceVersion(hard[curSellComboDevices]).c_str());
+
+		auto editDeviceType =
+			static_cast<CEdit*>(this->GetDlgItem(IDC_EDIT_DEVICE_TYPE));
+		editDeviceType->SetWindowTextW(info.GetDeviceType(hard[curSellComboDevices]).c_str());
 	}
 
 }
